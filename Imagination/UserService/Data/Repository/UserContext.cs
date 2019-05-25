3e7b974902c7e823fb060a974dec13025d26e159
Julia Matveeva <juamatveeva@gmail.com>
2019-05-25 19:40:36 +0300

    users db created

---
+++ Imagination/UserService/Data/Repository/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Data.Models;
+
+namespace UserService.Data.Repository
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserBoard> UserBoards { get; set; }
+        public DbSet<BoardImage> BoardImages { get; set; }
+
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
