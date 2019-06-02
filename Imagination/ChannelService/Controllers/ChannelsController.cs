ee12607097b2c293033f4aedd3f7a17a319ff89a
Julia Matveeva <juamatveeva@gmail.com>
2019-06-02 22:31:12 +0300

    controllers added

---
+++ Imagination/ChannelService/Controllers/ChannelsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChannelService.Data.Models;
+using ChannelService.Data.Repository;
+
+namespace ChannelService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChannelsController : ControllerBase
+    {
+        private readonly ChannelContext _context;
+
+        public ChannelsController(ChannelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Channels
+        [HttpGet]
+        public IEnumerable<Channel> GetChannels([FromQuery] int userid)
+        {
+            return _context.Channels.Where(c => c.UserId == userid);
+        }
+
+        // GET: api/Channels/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetChannel([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var channel = await _context.Channels.FindAsync(id);
+
+            if (channel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(channel);
+        }
+
+        // PUT: api/Channels/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutChannel([FromRoute] int id, [FromBody] Channel channel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != channel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(channel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChannelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Channels
+        [HttpPost]
+        public async Task<IActionResult> PostChannel([FromBody] Channel channel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Channels.Add(channel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetChannel", new { id = channel.Id }, channel);
+        }
+
+        // DELETE: api/Channels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteChannel([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var channel = await _context.Channels.FindAsync(id);
+            if (channel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Channels.Remove(channel);
+            await _context.SaveChangesAsync();
+
+            return Ok(channel);
+        }
+
+        private bool ChannelExists(int id)
+        {
+            return _context.Channels.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Imagination/ChannelService/Controllers/SubscriptionsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ChannelService.Data.Models;
+using ChannelService.Data.Repository;
+
+namespace ChannelService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionsController : ControllerBase
+    {
+        private readonly ChannelContext _context;
+
+        public SubscriptionsController(ChannelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Subscriptions
+        [HttpGet]
+        public IEnumerable<SubsResponse> GetChannelSubscribers([FromQuery] int userid)
+        {
+            var subs = _context.Subscriptions.Where(s => s.UserId == userid);
+            var resultSubs = new List<SubsResponse>();
+            foreach (var sub in subs)
+            {
+                var channelname = _context.Channels.Find(sub.ChannelId).Name;
+                resultSubs.Add(new SubsResponse()
+                {
+                    Id = sub.Id,
+                    UserId = sub.UserId,
+                    ChannelName = channelname
+                });
+            }
+            return resultSubs;
+        }
+
+        // GET: api/Subscriptions/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSubscription([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var subscription = await _context.Subscriptions.FindAsync(id);
+
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subscription);
+        }
+
+        // PUT: api/Subscriptions/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubscription([FromRoute] int id, [FromBody] Subscription subscription)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != subscription.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subscription).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubscriptionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Subscriptions
+        [HttpPost]
+        public async Task<IActionResult> PostSubscription([FromBody] Subscription subscription)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Subscriptions.Add(subscription);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSubscription", new { id = subscription.Id }, subscription);
+        }
+
+        // DELETE: api/Subscriptions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSubscription([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var subscription = await _context.Subscriptions.FindAsync(id);
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            _context.Subscriptions.Remove(subscription);
+            await _context.SaveChangesAsync();
+
+            return Ok(subscription);
+        }
+
+        private bool SubscriptionExists(int id)
+        {
+            return _context.Subscriptions.Any(e => e.Id == id);
+        }
+    }
+}
