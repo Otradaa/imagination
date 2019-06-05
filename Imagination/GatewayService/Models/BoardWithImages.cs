2bca69e28e8a74d7583c8b25c59988d41cbcc99e
Julia Matveeva <juamatveeva@gmail.com>
2019-06-05 16:30:19 +0300

    view models of board and channel

---
+++ Imagination/GatewayService/Models/BoardWithImages.cs
@@ -0,0 +1,16 @@
+﻿using StorageService.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Data.Models;
+
+namespace GatewayService.Models
+{
+    public class BoardWithImages
+    {
+        public UserBoard board { get; set; }
+        public IEnumerable<BoardImage> boardimages { get; set; }
+        public IEnumerable<Image> images { get; set; }
+    }
+}
---
+++ Imagination/GatewayService/Models/ChannelWithImages.cs
@@ -0,0 +1,17 @@
+﻿using ChannelService.Data.Models;
+using StorageService.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GatewayService.Models
+{
+    public class ChannelWithImages
+    {
+        public Channel channel { get; set; }
+        public IEnumerable<ChannelImage> images { get; set; }
+        public IEnumerable<Image> files { get; set; }
+        public int subsCount { get; set; }
+    }
+}
